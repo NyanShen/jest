e36746608e962c5bce5e0a793566c269c30cbdd2
NyanShen <sibaoemail@126.com>
2019-08-02 22:46:42 +0800

    init jest math test

---
+++ math.test.js
@@ -0,0 +1,15 @@
+// node 遵循CommonJs规范, 需要babel内部转化
+const math = require("./math");
+const { add, minus, multi } = math;
+
+test("test add method 4 + 6 expect to 10", () => {
+    expect(add(4, 6)).toBe(10)
+})
+
+test("test minus method 4 - 6 expect to -2", () => {
+    expect(minus(4, 6)).toBe(-2)
+})
+
+test("test add method 4 * 6 expect to 24", () => {
+    expect(multi(4, 6)).toBe(24)
+})
